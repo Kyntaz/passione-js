87e02b37d82521584a46c1664a39cfda29ca4ff2
Kyntaz <1260pedroquintas@gmail.com>
2022-05-29 00:34:38 +0100

    Minify generated HTML

--- src/GeneratePage.ts
+++ src/GeneratePage.ts
@@ -1,5 +1,8 @@
 import { generateBlocks } from "./GenerateBlocks.js";
+import { minify } from "html-minifier";
 
 export async function generatePage(page: string, blocksPath: string) {
-    return generateBlocks(page, blocksPath);
+    return minify(await generateBlocks(page, blocksPath), {
+        collapseWhitespace: true,
+    });
 }
